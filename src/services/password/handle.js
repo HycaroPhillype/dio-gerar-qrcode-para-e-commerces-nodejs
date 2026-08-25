16e201b6f8775500d10224b1143e31bc6f9056d4
Hycaro Phillype <phillypehycaro@gmail.com>
2026-08-24 22:09:37 -0300

    feat: Refactoring cod..

--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,20 +1,3 @@
-async function permittedCharacters() {
-  let permitted = [];
-
-  if (process.env.UPPERCASE_LETTERS === "true")
-    permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
-
-  if (process.env.LOWERCASE_LETTERS === "true")
-    permitted.push(..."abcdefghijklmnopqrstuvwxyz".split(""));
-
-  if (process.env.NUMBERS === "true") permitted.push(..."0123456789".split(""));
-
-  if (process.env.SPECIAL_CHARACTERS === "true")
-    permitted.push(..."!@#$%^&*()-+".split(""));
-
-  return permitted;
-}
-
 async function handle() {
   let caracters = [];
   let password = "";
---
+++ src/services/password/utils/permitted-characters.js
@@ -0,0 +1,20 @@
+import permittedCharacters from "./utils/permitted-characters.js";
+
+async function permittedCharacters() {
+  let permitted = [];
+
+  if (process.env.UPPERCASE_LETTERS === "true")
+    permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
+
+  if (process.env.LOWERCASE_LETTERS === "true")
+    permitted.push(..."abcdefghijklmnopqrstuvwxyz".split(""));
+
+  if (process.env.NUMBERS === "true") permitted.push(..."0123456789".split(""));
+
+  if (process.env.SPECIAL_CHARACTERS === "true")
+    permitted.push(..."!@#$%^&*()-+".split(""));
+
+  return permitted;
+}
+
+export default permittedCharacters;
